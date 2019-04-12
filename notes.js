df146235c1738fdf91ba364c19e3671e98fe980d
Jay Arteficio <jarteficio@gmail.com>
2019-04-12 14:38:09 -0700

    first commit(really)

---
+++ notes.js
@@ -0,0 +1,12 @@
+Use to learn sockets and prep for chat app
+
+1. > npm init
+    entry point is Server/JS
+2. Dependencies will be socket.io and monggodb
+
+    > npm install mongodb socket.io --save
+
+3. touch server.js
+   require the 2 dependencies
+
+   
